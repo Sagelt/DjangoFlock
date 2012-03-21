59b6a818551f3023c4efd8f8c024b10c01825dc4
Kit La T <kit.la.t@gmail.com>
2012-03-21 12:32:01 -0600

    Limited what data is shown from User models.

--- events/models.py
+++ events/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User
 from datetime import datetime
+from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
@@ -17,14 +17,22 @@ class Event(models.Model):
     end = models.DateTimeField()
     min = models.IntegerField()
     max = models.IntegerField()
+    
     def add_player(self, user):
         if user not in self.players and len(self.players) < self.max:
             self.players.add(user)
             return True
         return False
+    
     def remove_player(self, user):
         self.players.remove(user)
+    
     def is_ready(self):
         return len(self.players) >= self.min
+    
     def is_ongoing(self):
         return self.start <= datetime.now() <= self.end
+    
+    @property
+    def duration(self):
+        return self.end - self.start
--- events/resources.py
+++ events/resources.py
@@ -1,9 +1,16 @@
+from django.contrib.auth.models import User
 from djangorestframework.resources import ModelResource
 from events.models import Game, Event
+
+class UserResource(ModelResource):
+    model = User
+    fields = ('username', 'first_name', 'last_name')
 
 class GameResource(ModelResource):
     model = Game
     
 class EventResource(ModelResource):
     model = Event
-
+    fields = (('gm', UserResource),
+              ('players', UserResource), ('game', GameResource),
+              'start', 'end', 'min', 'max')
