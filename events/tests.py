acc9e2abc2f6e7309256b349f12455bf8a6b8334
Kit La T <kit.la.t@gmail.com>
2012-03-23 19:38:22 -0600

    Added preliminary unit tests.

--- events/tests.py
+++ events/tests.py
@@ -2,7 +2,9 @@
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
+These tests aren't very good, yet. Still working on what makes sense to test,
+how to deal with database integration tests. Should also write some selenium
+tests soon.
 """
 
 from django.test import TestCase
@@ -35,19 +37,47 @@ class GameTest(TestCase):
         # logged in and with good data
         response = self.client.post('/games/', {'name': 'Fiasco'})
         assert response.status_code == 201
-        self.assertQuerysetEqual(Game.objects.filter(name='Fiasco'), ['<Game: Fiasco>'])
+        self.assertQuerysetEqual(Game.objects.filter(name='Fiasco'),
+                                 ['Fiasco'], lambda m: m.name)
     
     def test_list(self):
-        pass
+        response = self.client.get('/games/',
+                                   **{'Accept': 'application/json',
+                                      'CONTENT_TYPE': 'application/json'})
+        assert response.status_code == 200
+        # TODO some assertion that there are an appropriate n items in the
+        # response. Should probably get plaintext or json response, then.
+        # Unclear why the code above isn't getting a json response.
+        #assert
     
     def test_retrieve(self):
-        pass
+        Game(name='Fiasco').save()
+        response = self.client.get('/games/1/')
+        assert response.status_code == 200
     
     def test_update(self):
-        pass
+        Game(name='Fiasco').save()
+        # Not logged in
+        response = self.client.put('/games/1/', {'name': 'Fiascox'})
+        assert response.status_code == 403
+        assert len(Game.objects.filter(name='Fiascox')) == 0
+        # Logged in
+        self.client.login(username='kit', password='foobar')
+        response = self.client.put('/games/1/', {'name': 'Fiascox'})
+        assert response.status_code == 200
+        assert len(Game.objects.filter(name='Fiascox')) == 1
     
     def test_delete(self):
-        pass
+        Game(name='Fiasco').save()
+        # Not logged in
+        response = self.client.delete('/games/1/')
+        assert response.status_code == 403
+        assert len(Game.objects.filter(name='Fiasco')) == 1
+        # Logged in
+        self.client.login(username='kit', password='foobar')
+        response = self.client.delete('/games/1/')
+        assert response.status_code == 204
+        assert len(Game.objects.filter(name='Fiasco')) == 0
 
 class EventTest(TestCase):
     def setUp(self):
