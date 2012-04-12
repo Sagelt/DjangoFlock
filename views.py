41910194fe1c36c9f1e3a19c774a2ba757247e47
Kit La T <kit.la.t@gmail.com>
2012-04-11 18:12:10 -0600

    Added skeletons for templates for HTML UI.

--- views.py
+++ views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from events.models import Publisher, Game, Event
 
 def home(request):
     return render_to_response('base.html')
@@ -10,19 +11,25 @@ def register_view(request):
     return render_to_response('signup.html')
 
 def publishers_list(request):
-    pass
+    publishers = Publisher.objects.all()
+    return render_to_response('publishers_list.html', {'publishers': publishers})
 
 def publishers_instance(request, pk):
-    pass
+    publisher = Publisher.objects.get(pk=pk)
+    return render_to_response('publishers_instance.html', {'publisher': publisher})
 
 def games_list(request):
-    pass
+    games = Game.objects.all()
+    return render_to_response('games_list.html', {'games': games})
 
 def games_instance(request, pk):
-    pass
+    game = Game.objects.get(pk=pk)
+    return render_to_response('games_instance.html', {'game': game})
 
 def events_list(request):
-    pass
+    events = Event.objects.all()
+    return render_to_response('events_list.html', {'events': events})
 
 def events_instance(request, pk):
-    pass
+    event = Event.objects.get(pk=pk)
+    return render_to_response('events_instance.html', {'event': event})
