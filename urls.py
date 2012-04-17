8324c6d9e5f2766a34a5d6f09f22f6120623e075
Kit La T <kit.la.t@gmail.com>
2012-04-16 18:15:55 -0600

    Added skeleton URL structure for desktop interface.

--- urls.py
+++ urls.py
@@ -24,11 +24,32 @@ urlpatterns = patterns('',
 
     # HTML forms for accessing the REST API.
     url('^publishers/$', views.publishers_list),
+    url('^publishers/new/$', views.publishers_new),
     url('^publishers/(?P<pk>[^/]+)/$', views.publishers_instance),
+    url('^publishers/(?P<pk>[^/]+)/edit/$', views.publishers_instance_edit),
+
     url('^games/$', views.games_list),
+    url('^games/new/$', views.games_new),
     url('^games/(?P<pk>[^/]+)/$', views.games_instance),
+    url('^games/(?P<pk>[^/]+)/edit/$', views.games_instance_edit),
+
+    url('^conventions/$', views.conventions_list),
+    url('^conventions/new/$', views.conventions_new),
+    url('^conventions/(?P<pk>[^/]+)/$', views.conventions_instance),
+    url('^conventions/(?P<pk>[^/]+)/edit/$', views.conventions_instance_edit),
+
     url('^events/$', views.events_list),
+    url('^events/new/$', views.events_new),
     url('^events/(?P<pk>[^/]+)/$', views.events_instance),
+    url('^events/(?P<pk>[^/]+)/edit/$', views.events_instance_edit),
+    url('^events/(?P<pk>[^/]+)/join/$', views.events_instance_join),
+    url('^events/(?P<pk>[^/]+)/leave/$', views.events_instance_leave),
+
+    url('^demands/$', views.demands_list),
+    url('^demands/mine/$', views.demands_list_mine),
+    url('^demands/new/$', views.demands_new),
+    url('^demands/(?P<pk>[^/]+)/$', views.demands_instance),
+    url('^demands/(?P<pk>[^/]+)/edit/$', views.demands_instance_edit),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- views.py
+++ views.py
@@ -7,8 +7,6 @@ from events.models import Publisher, Game, Event
 def home(request):
     if request.flavour == 'mobile':
         return render_to_response("mobile/mobile.html")
-    # @todo: Next line just for testing.
-    return render_to_response("mobile/mobile.html")
     return render_to_response('base.html')
 
 def register_view(request):
@@ -22,7 +20,19 @@ def publishers_list(request):
         return render_to_response("mobile/mobile.html")
     return render_to_response('publishers_list.html', {'publishers': publishers})
 
+def publishers_new(request):
+    publishers = Publisher.objects.all()
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('publishers_list.html', {'publishers': publishers})
+
 def publishers_instance(request, pk):
+    publisher = Publisher.objects.get(pk=pk)
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('publishers_instance.html', {'publisher': publisher})
+
+def publishers_instance_edit(request, pk):
     publisher = Publisher.objects.get(pk=pk)
     if request.flavour == 'mobile':
         return render_to_response("mobile/mobile.html")
@@ -34,13 +44,43 @@ def games_list(request):
         return render_to_response("mobile/mobile.html")
     return render_to_response('games_list.html', {'games': games})
 
+def games_new(request):
+    games = Game.objects.all()
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('games_list.html', {'games': games})
+
 def games_instance(request, pk):
     game = Game.objects.get(pk=pk)
     if request.flavour == 'mobile':
         return render_to_response("mobile/mobile.html")
     return render_to_response('games_instance.html', {'game': game})
 
+def games_instance_edit(request, pk):
+    game = Game.objects.get(pk=pk)
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('games_instance.html', {'game': game})
+
+def conventions_list(request):
+    raise NotImplementedError
+
+def conventions_new(request):
+    raise NotImplementedError
+
+def conventions_instance(request, pk):
+    raise NotImplementedError
+
+def conventions_instance_edit(request, pk):
+    raise NotImplementedError
+
 def events_list(request):
+    events = Event.objects.all()
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('events_list.html', {'events': events})
+
+def events_new(request):
     events = Event.objects.all()
     if request.flavour == 'mobile':
         return render_to_response("mobile/mobile.html")
@@ -51,3 +91,36 @@ def events_instance(request, pk):
     if request.flavour == 'mobile':
         return render_to_response("mobile/mobile.html")
     return render_to_response('events_instance.html', {'event': event})
+
+def events_instance_edit(request, pk):
+    event = Event.objects.get(pk=pk)
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('events_instance.html', {'event': event})
+
+def events_instance_join(request, pk):
+    event = Event.objects.get(pk=pk)
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('events_instance.html', {'event': event})
+
+def events_instance_leave(request, pk):
+    event = Event.objects.get(pk=pk)
+    if request.flavour == 'mobile':
+        return render_to_response("mobile/mobile.html")
+    return render_to_response('events_instance.html', {'event': event})
+
+def demands_list(request):
+    raise NotImplentedError
+
+def demands_list_mine(request):
+    raise NotImplentedError
+
+def demands_new(request):
+    raise NotImplentedError
+
+def demands_instance(request, pk):
+    raise NotImplentedError
+
+def demands_instance_edit(request, pk):
+    raise NotImplentedError
